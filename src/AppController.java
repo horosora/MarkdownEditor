c40795e483215ed9db864ac05422fc49c2edf233
horosora <horosora@gmail.com>
2017-09-21 21:33:48 +0900

    update

--- src/AppController.java
+++ src/AppController.java
@@ -1,19 +1,38 @@
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import org.markdown4j.Markdown4jProcessor;
 
-public class AppController {
+public class AppController implements Initializable {
 
-    @FXML
-    private WebView webView;
+    @FXML private WebView webView;
+    @FXML private TextArea textArea;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textArea.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                webView.getEngine().loadContent(new Markdown4jProcessor().process(newValue));
+            }
+            catch (StringIndexOutOfBoundsException | IOException e) {
+                webView.getEngine().loadContent(newValue);
+            }
+        });
+    }
 
     @FXML
     public void fileOpen(Event e){
@@ -31,6 +50,7 @@ public class AppController {
         System.exit(0);
     }
 
+
     @FXML
     public void fileSaveAa(Event e){
         FileChooser saveSelect = new FileChooser();
@@ -38,7 +58,14 @@ public class AppController {
         saveSelect.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("markdown file", "*.md"),
                                                 new FileChooser.ExtensionFilter("All Files", "*.*"));
 
-        File importFile = saveSelect.showSaveDialog(null);
+        try{
+            File saveFile = saveSelect.showSaveDialog(null);
+            FileWriter filewriter = new FileWriter(saveFile);
+            filewriter.write(textArea.getText());
+            filewriter.close();
+        }catch(IOException er){
+            System.out.println(er);
+        }
     }
 
     @FXML
